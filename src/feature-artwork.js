744b71b6d9d202026f0451b7c021b1283adf5cad
Brady Vercher <brady@blazersix.com>
2014-07-29 11:48:50 -0700

    Toggle a '.has-artwork' class on the playlist.

--- src/feature-artwork.js
+++ src/feature-artwork.js
@@ -7,10 +7,14 @@
 			var $artwork = layers.append( '<span class="mejs-track-artwork"><img src=""></span>' ).find( '.mejs-track-artwork' );
 
 			player.$node.on( 'setTrack.cue', function( e, track, player ) {
+				var hasArtwork;
+
 				track.thumb = track.thumb || {};
+				hasArtwork = 'undefined' !== typeof track.thumb.src && '' !== track.thumb.src;
 
 				// Set the artwork src and toggle depending on if the URL is empty.
-				$artwork.find( 'img' ).attr( 'src', track.thumb.src ).toggle( 'undefined' !== typeof track.thumb.src && '' !== track.thumb.src );
+				$artwork.find( 'img' ).attr( 'src', track.thumb.src ).toggle( hasArtwork );
+				$artwork.closest( player.options.cueSelectors.playlist ).toggleClass( 'has-artwork', hasArtwork );
 			});
 		}
 
