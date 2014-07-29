257a0eb60ee31bdc5972a2a80c9a17f2e11274bc
Brady Vercher <brady@blazersix.com>
2014-07-29 12:16:57 -0700

    Add an icons feature to move icons from the template into the player's controls once it's been initialized. Useful for inline SVGs.

---
+++ src/feature-icons.js
@@ -0,0 +1,16 @@
+(function( window, $, cue, undefined ) {
+	'use strict';
+
+	// Add this feature after all controls have been built.
+	$.extend( MediaElementPlayer.prototype, {
+		buildcueicons: function( player, controls ) {
+			var $icons = $( player.options.cueSelectors.container ).find( '[data-cue-control]' );
+
+			$icons.each(function() {
+				var $icon = $( this );
+				$icon.appendTo( controls.find( $icon.data( 'cueControl' ) ) );
+			});
+		}
+	});
+
+})( this, jQuery, window.cue );
