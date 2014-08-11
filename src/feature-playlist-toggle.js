dcac5cad97e4dfe3bd6db59e62f7fc698b433dc0
Brady Vercher <brady@blazersix.com>
2014-08-11 15:55:44 -0700

    Toggle open/closed classes on the button and container element when the track list is toggled, and add a callback option for changing the toggle method.

--- src/feature-playlist-toggle.js
+++ src/feature-playlist-toggle.js
@@ -1,16 +1,40 @@
 (function( window, $, cue, undefined ) {
 	'use strict';
 
+	$.extend( mejs.MepDefaults, {
+		cuePlaylistToggle: function( $tracklist, player ) {
+			$tracklist.slideToggle( 200 );
+		}
+	});
+
 	$.extend( MediaElementPlayer.prototype, {
 		buildcueplaylisttoggle: function( player, controls, layers, media ) {
-			var selectors = player.options.cueSelectors;
+			var selectors = player.options.cueSelectors,
+				$playlist = player.container.closest( selectors.playlist ),
+				$tracklist = $playlist.find( selectors.tracklist ),
+				isTracklistVisible = $tracklist.is( ':visible' );
+
+			$playlist.addClass(function() {
+				return isTracklistVisible ? 'is-tracklist-open' : 'is-tracklist-closed';
+			});
 
 			$( '<div class="mejs-button mejs-toggle-playlist-button mejs-toggle-playlist">' +
 				'<button type="button" aria-controls="' + player.id + '" title="' + cue.l10n.togglePlaylist + '"></button>' +
 				'</div>' )
 			.appendTo( player.controls )
 			.on( 'click', function() {
-				$( this ).closest( selectors.playlist ).find( selectors.tracklist ).slideToggle( 200 );
+				var $button = $( this ),
+					isTracklistVisible = $tracklist.is( ':visible' );
+
+				$button.toggleClass( 'is-open', ! isTracklistVisible ).toggleClass( 'is-closed', isTracklistVisible );
+				$playlist.toggleClass( 'is-tracklist-open', ! isTracklistVisible ).toggleClass( 'is-tracklist-closed', isTracklistVisible );
+
+				if ( $.isFunction( player.options.cuePlaylistToggle ) ) {
+					player.options.cuePlaylistToggle( $tracklist, player );
+				}
+			})
+			.addClass(function() {
+				return isTracklistVisible ? 'is-open' : 'is-closed';
 			});
 		}
 	});
