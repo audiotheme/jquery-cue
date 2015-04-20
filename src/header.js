72a2a8d968989e0a4513acafdc54b70c87e563d1
Brady Vercher <brady@blazersix.com>
2015-04-20 15:26:19 -0700

    Update the banner URL to https.

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 /*!
  * jquery.cue.js - <%= pkg.version %>
  * Playlist and other functionality for MediaElement.js
- * http://audiotheme.com/
+ * https://audiotheme.com/
  *
  * Copyright 2014, AudioTheme LLC
  * License: MIT
